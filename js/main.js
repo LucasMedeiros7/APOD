914c244b73c25e79e765016dce74b83c4693fa33
Lucas Medeiros <lucaasmedeiros7@gmail.com>
2022-05-25 18:43:30 -0300

    :boom: feat: codigo com utilização da lib 'jquery'

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$('form').submit(false);
+
+const div = $('figure');
+const h2 = $('#titulo');
+const p = $('#texto');
+const datahtml = $('#date')
+
+function apod() {
+
+  let date = $('#data').val();
+
+  $.ajax({
+    url: `https://api.nasa.gov/planetary/apod?api_key=Iy0QFduErGyyBEM7saLdYK0ZtpfW35a1JfJtiCl0date=${date}`,
+    success(resposta) {
+      let titulo = resposta.title;
+      let data = resposta.date;
+      let texto = resposta.explanation;
+      let img = resposta.url;
+      let video = resposta.media_type;
+
+      h2.html(titulo);
+      p.html(texto);
+      datahtml.html(data);
+
+      if (video === 'video') {
+        div.html(`<iframe src="${video}" frameborder="0"  allowfullscreen=""></iframe>`)
+      } else {
+        div.html(`<img src="${img}">`)
+      }
+
+
+    }
+  })
+}
+
+apod();
